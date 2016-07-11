ce6cdd3ca74d0ac4210ca8207d3a6d8c38ea8ed5
patternproject <patternproject@yahoo.com>
2016-07-11 22:07:44 +0500

    small changes to script file

--- 3. Scripts/v 0 0 1.R
+++ 3. Scripts/v 0 0 1.R
@@ -1,11 +1,36 @@
 ## `````````````````````````````````````````````
 #### Read Me ####
 ## `````````````````````````````````````````````
+## Make over Monday Entry for Wk 28
 ## `````````````````````````````````````````````
 
 ## `````````````````````````````````````````````
 #### Load Libraries ####
 ## `````````````````````````````````````````````
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr,ggplot2,tidyr,scales,grid,stringr,rvest)
+pacman::p_load(RColorBrewer)
+#pacman::p_load(ellipse)
+pacman::p_load(ggthemes)
+pacman::p_load(stats)
+pacman::p_load(plotly)
+#pacman::p_load(ggnet)
+#pacman::p_load(ggseas)
+#pacman::p_load(forecast)
+#pacman::p_load(moments) # for kurtosis
+#pacman::p_load(Lahman)
+#pacman::p_load(zoo)
+#pacman::p_load(ggseas)
+#pacman::p_load(RcppRoll)
+#pacman::p_load(stringdist)
+#pacman::p_load(maps)
+#pacman::p_load(choroplethr)
+#pacman::p_load(choroplethrMaps)
+#pacman::p_load(ggmap)
+#pacman::p_load(mapdata)
+#pacman::p_load(GGally)
+#pacman::p_load(car)
+
 ## `````````````````````````````````````````````
 
 ## `````````````````````````````````````````````
